85a818e48816cbfb8dce21e8da03d02e93163d2c
viniciuslemesz <vinileme0853@gmail.com>
2025-07-05 14:17:45 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <label for="nome">
+            NOME:
+        </label>
+    </div>
+
+
+</body>
+</html>
