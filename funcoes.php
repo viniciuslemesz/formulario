4fcc42c6efc47d4681bb6651f929a5bf4e07c02f
viniciuslemesz <vinileme0853@gmail.com>
2025-07-05 15:36:54 -0300

    terceiro commit

---
+++ funcoes.php
@@ -0,0 +1,25 @@
+<?php
+//validações do formulario
+
+$erro = 0;
+function formulario_nao_enviado() {
+    return $_SERVER['REQUEST_METHOD'] !== 'POST';
+    $erro = 1;
+}
+
+function formulario_em_branco() {
+    return $_SERVER['REQUEST_METHOD'] == 'empty';
+}
+
+function idade_invalido(){
+    $idade = $_POST['idade'];
+
+    if($idade < 0 || $idade > 99)
+    {
+        echo '<h3>Preencha idade com um número válido!</h3>';
+
+    }
+}
+
+
+?>
